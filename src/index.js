394ff0aeffcc8ccdb08ea9dd0967caa2cba706a4
WPNQ <102561944+WPNQ@users.noreply.github.com>
2025-04-04 20:43:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.css';
 import GistGlance from './components/GistGlance';
 
-ReactDOM.render(<GistGlance />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <GistGlance />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
